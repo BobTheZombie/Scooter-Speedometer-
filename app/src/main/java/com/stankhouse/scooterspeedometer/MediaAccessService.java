271019c90f57db04cb62962f1302e07cad1abd53
BobTheZombie <dmidkiff47021@gmail.com>
2026-09-02 20:40:18 -0400

    Add media-session access service

---
+++ app/src/main/java/com/stankhouse/scooterspeedometer/MediaAccessService.java
@@ -0,0 +1,6 @@
+package com.stankhouse.scooterspeedometer;
+
+import android.service.notification.NotificationListenerService;
+
+/** Authorizes the activity to access Android's active media sessions. */
+public class MediaAccessService extends NotificationListenerService { }
