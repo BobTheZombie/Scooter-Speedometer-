5f24c895c81437e981e5b6aed2fee2bff2151c7a
BobTheZombie <dmidkiff47021@gmail.com>
2026-09-13 16:30:42 -0400

    Rename built-in navigation to OpenNAV+

--- app/src/main/java/com/stankhouse/scooterspeedometer/MainActivity.java
+++ app/src/main/java/com/stankhouse/scooterspeedometer/MainActivity.java
@@ -413,7 +413,7 @@ public class MainActivity extends Activity implements LocationListener {
     }
 
     private void chooseNavigationApp(String destination) {
-        String[] apps = {"Built-in free navigation", "Google Maps", "Waze", "Another navigation app"};
+        String[] apps = {"OpenNAV+", "Google Maps", "Waze", "Another navigation app"};
         new AlertDialog.Builder(this)
                 .setTitle("Navigate with")
                 .setItems(apps, (dialog, which) -> launchNavigation(destination, which))
